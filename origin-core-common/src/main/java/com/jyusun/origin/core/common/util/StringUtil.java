327cc9545db2bcf030b90dd6a5db69d3297e099c
sunjianyu <sunjianyu@ysstech.com>
2021-07-26 13:58:02 +0800

    驼峰转下划线，下划线转驼峰

--- origin-core-common/src/main/java/com/jyusun/origin/core/common/util/StringUtil.java
+++ origin-core-common/src/main/java/com/jyusun/origin/core/common/util/StringUtil.java
@@ -1,7 +1,5 @@
 package com.jyusun.origin.core.common.util;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.StringUtils;
 
@@ -65,5 +63,44 @@ public final class StringUtil extends StringUtils {
     public static final String HTML_GT = "&gt;";
 
     public static final String EMPTY_JSON = "{}";
+
+    /**
+     * 下划线转驼峰
+     *
+     * @param para 字符串
+     * @return String
+     */
+    public static String underlineToHump(String para) {
+        StringBuilder result = new StringBuilder();
+        String[] a = para.split(UNDERLINE);
+        for (String s : a) {
+            if (result.length() == 0) {
+                result.append(s.toLowerCase());
+            } else {
+                result.append(s.substring(0, 1).toUpperCase());
+                result.append(s.substring(1).toLowerCase());
+            }
+        }
+        return result.toString();
+    }
+
+    /**
+     * 驼峰转下划线
+     *
+     * @param para 字符串
+     * @return String
+     */
+    public static String humpToUnderline(String para) {
+        StringBuilder sb = new StringBuilder(para);
+        int temp = 0;
+        for (int i = 0; i < para.length(); i++) {
+            if (Character.isUpperCase(para.charAt(i))) {
+                sb.insert(i + temp, "_");
+                temp += 1;
+            }
+        }
+        return sb.toString().toLowerCase();
+    }
+
 
 }
