a53fadc629873101cb3b0adcfd0777638d6fea1b
JyuSun <mr.evansun@foxmail.com>
2022-03-01 23:25:40 +0800

    Changes

---
+++ origin-core-common/src/main/java/com/jyusun/origin/core/common/constant/ServiceConstant.java
@@ -0,0 +1,39 @@
+package com.jyusun.origin.core.common.constant;
+
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * 服务名称常量
+ *
+ * @author jyusun
+ * @since 1.0.0
+ */
+@UtilityClass
+public class ServiceConstant implements BaseConstants {
+
+
+    /**
+     * 系统服务-认证服务
+     */
+    public static final String ORIGIN_SYSTEM_AUTH = "origin-system-auth";
+    /**
+     * 系统服务-用户服务
+     */
+    public static final String ORIGIN_SYSTEM_UPMS = "origin-system-upms";
+    /**
+     * 系统服务-数据字典
+     */
+    public static final String ORIGIN_SYSTEM_DICT = "origin-system-dict";
+    /**
+     * 系统服务-日志服务
+     */
+    public static final String ORIGIN_SYSTEM_LOGGER = "origin-system-logger";
+
+    /**
+     * 系统服务-基础信息
+     */
+    public static final String ORIGIN_SYSTEM_BASEINFO = "origin-system-baseinfo";
+
+
+}
