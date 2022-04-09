e3a203f77d055cb43493419196a977db8e694dca
sunjianyu <sunjianyu@ysstech.com>
2022-04-10 00:57:10 +0800

    init

--- origin-core-model/src/main/java/com/jyusun/origin/core/model/domain/event/AbstractEvent.java
+++ origin-core-model/src/main/java/com/jyusun/origin/core/model/domain/event/AbstractEvent.java
@@ -17,7 +17,7 @@ import java.io.Serializable;
 @Accessors(chain = true)
 @EqualsAndHashCode
 @NoArgsConstructor
-public class AbstractEvent<T extends Serializable> implements Serializable {
+public class AbstractEvent<T extends Serializable> implements DomainEvent, Serializable {
 
     private Serializable sid;
 
@@ -25,4 +25,45 @@ public class AbstractEvent<T extends Serializable> implements Serializable {
 
     private T source;
 
+    AbstractEvent(T source) {
+        this.source = source;
+        this.init();
+    }
+
+    private void init() {
+        this.sid = 1L;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+
+    /**
+     * 返回事件的唯一ID
+     *
+     * @return 事件的唯一ID
+     */
+    @Override
+    public Serializable sid() {
+        return this.sid;
+    }
+
+    /**
+     * 返回事件的产生的时间
+     *
+     * @return 事件的产生的时间
+     */
+    @Override
+    public long timestamp() {
+        return this.timestamp;
+    }
+
+    /**
+     * 判断领域事件是否一致
+     *
+     * @param other 另外一个领域事件
+     * @return <code>true</code> 如果当前比较的两个领域事件一致返回true
+     */
+    @Override
+    public boolean sameEventAs(DomainEvent other) {
+        return false;
+    }
 }
---
+++ origin-core-model/src/main/java/com/jyusun/origin/core/model/domain/event/DomainEvent.java
@@ -0,0 +1,32 @@
+package com.jyusun.origin.core.model.domain.event;
+
+import java.io.Serializable;
+
+/**
+ * 领域事件可能有唯一标识
+ *
+ * @author jyusun at 2022-04-10 00:48:16
+ */
+public interface DomainEvent {
+    /**
+     * 返回事件的唯一ID
+     *
+     * @return 事件的唯一ID
+     */
+    Serializable sid();
+
+    /**
+     * 返回事件的产生的时间
+     *
+     * @return 事件的产生的时间
+     */
+    long timestamp();
+
+    /**
+     * 判断领域事件是否一致
+     *
+     * @param other 另外一个领域事件
+     * @return <code>true</code> 如果当前比较的两个领域事件一致返回true
+     */
+    boolean sameEventAs(DomainEvent other);
+}
